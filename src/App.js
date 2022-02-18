ca8958b5e39d7cd7522f59703cb000ecaa36cfab
DRucci-88 <60127414+DRucci-88@users.noreply.github.com>
2022-02-18 12:36:40 +0700

    .editorconfig tab-indent: 2

--- src/App.js
+++ src/App.js
@@ -4,29 +4,29 @@ import Button from "./Button";
 import TopNavbar from "./TopNavbar";
 
 function App() {
-	return (
-		<div className="App">
-			<TopNavbar />
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<div className="flex">
-					<Button variant="primary" className="mr-4">Button Prime</Button>
-					<Button variant="secondary">Button Sub</Button>
-				</div>
-			</header>
-		</div>
-	);
+  return (
+    <div className="App">
+      <TopNavbar/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <div className="flex">
+          <Button variant="primary" className="mr-4">Button Prime</Button>
+          <Button variant="secondary">Button Sub</Button>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
