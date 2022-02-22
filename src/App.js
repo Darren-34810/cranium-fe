73e102ac8ba9ca3a167399f294040637f6b9e182
Darren-34810 <darren.denisson@student.umn.ac.id>
2022-02-22 13:25:02 +0700

    Added Input component and Routing
    - Temporary routing
    - Button styles moved to css (reusable purpose)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Button from "./components/Button";
 import TopNavbar from "./components/TopNavbar";
 import EventCard from "./components/EventCard";
@@ -10,36 +11,70 @@ import Image from "./components/Image";
 import SpeakerCard from "./components/SpeakerCard";
 import ScheduleCard from "./components/ScheduleCard";
 import BoothBanner from "./components/BoothBanner";
+import Welcome from "./pages/welcome";
 
 function App() {
   return (
-    <div className="App">
-      <TopNavbar/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div className="flex">
-          <Button variant="primary">Button Prime</Button>
-          <Button variant="secondary" className="ml-4">Button Sub</Button>
-          <Button variant="tertiary" className="ml-4">Button Ter</Button>
+    <>
+      <div className="App">
+        <TopNavbar />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <div className="flex">
+            <Button variant="primary">Button Prime</Button>
+            <Button variant="secondary" className="ml-4">Button Sub</Button>
+            <Button variant="tertiary" className="ml-4">Button Ter</Button>
+          </div>
+        </header>
+        <Image />
+        <SpeakerCard />
+        <Input />
+        <ScheduleCard />
+        <BoothBanner variant="quarter" />
+      </div>
+
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/" element={<Welcome />} />
+            {/* <Route exact path="/homepage" element={<Homepage />} />
+            <Route exact path="/event" element={<EventPage />} />
+            <Route exact path="/eventlanding" element={<EventLandingPage />} />
+            <Route exact path="/signin" element={<SignIn />} />
+            <Route exact path="/signup" element={<SignUp />} />
+            <Route exact path="/dashboard" element={<DashboardOverview />} />
+            <Route exact path="/dashboard/overview" element={<DashboardOverview />} />
+            <Route exact path="/dashboard/events" element={<DashboardEvents />} />
+            <Route exact path="/dashboard/create" element={<DashboardCreate />} />
+            <Route exact path="/dashboard/billing" element={<DashboardBilling />} />
+            <Route exact path="/dashboard/integration" element={<DashboardIntegration />} />
+            <Route exact path="/dashboard/events" element={<DashboardEvents />} />
+            <Route exact path="/dashboard/profile" element={<DashboardProfile />} />
+            <Route exact path="/dashboard/events/overview" element={<EventDashboardOverview />} />
+            <Route exact path="/dashboard/events/setup" element={<EventDashboardBasicSetup />} />
+            <Route exact path="/dashboard/events/registration" element={<EventDashboardRegistration />} />
+            <Route exact path="/dashboard/events/schedule" element={<EventDashboardSchedule />} />
+            <Route exact path="/dashboard/events/venue" element={<EventDashboardVenue />} />
+            <Route exact path="/dashboard/events/people" element={<EventDashboardPeople />} />
+            <Route exact path="/dashboard/events/marketing" element={<EventDashboardMarketing />} />
+            <Route exact path="/dashboard/events/recording" element={<EventDashboardRecording />} />
+            <Route exact path="/dashboard/events/analytics" element={<EventDashboardAnalytics />} />
+            <Route path="*" element={<NotFound />} /> */}
+          </Switch>
         </div>
-      </header>
-      <Image/>
-      <SpeakerCard/>
-      <Input/>
-      <ScheduleCard/>
-      <BoothBanner variant="quarter"/>
-    </div>
+      </Router>
+    </>
   );
 }
 
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,7 +1,21 @@
-const Input=()=>{
-  return(
-   <div>
-   </div>
-  )
+const Input = ({type, className}) => {
+  const style = 'bg-neutral-700 border border-neutral-400 rounded text-base px-5 py-3 w-full'
+  if (type === "file") {
+    return (
+      <>
+      <label for="file-upload" className="btn-secondary">
+          Choose File
+      </label>
+      <input id="file-upload" type="file" className="hidden"/>
+      </>
+    )
+  }
+  else if (type === "textarea") {
+    return <textarea className={`${style} ${className ?? ''}`}></textarea>
+  }
+  else {
+    return <input type={type} className={`${style} ${className ?? ''}`} />
+  }
 }
+
 export default Input
