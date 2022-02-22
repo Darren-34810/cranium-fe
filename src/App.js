fd2e1c3a0b789f20b22cda5b103e18d75c2ed238
DRucci-88 <60127414+DRucci-88@users.noreply.github.com>
2022-02-22 22:06:07 +0700

    Sidebar under development

--- src/App.js
+++ src/App.js
@@ -13,75 +13,42 @@ import ScheduleCard from "./components/ScheduleCard";
 import BoothBanner from "./components/BoothBanner";
 import Welcome from "./pages/Welcome";
 import Dashboard from "./pages/testing/Dashboard";
+
 function App() {
   return (
-    <>
-      {/*<div className="App">*/}
-      {/*  <TopNavbar/>*/}
-      {/*  <header className="App-header">*/}
-      {/*    <img src={logo} className="App-logo" alt="logo"/>*/}
-      {/*    <p>*/}
-      {/*      Edit <code>src/App.js</code> and save to reload.*/}
-      {/*    </p>*/}
-      {/*    <a*/}
-      {/*      className="App-link"*/}
-      {/*      href="https://reactjs.org"*/}
-      {/*      target="_blank"*/}
-      {/*      rel="noopener noreferrer"*/}
-      {/*    >*/}
-      {/*      Learn React*/}
-      {/*    </a>*/}
-      {/*    <div className="flex">*/}
-      {/*      <Button variant="primary">Button Prime</Button>*/}
-      {/*      <Button variant="secondary" className="ml-4">Button Sub</Button>*/}
-      {/*      <Button variant="tertiary" className="ml-4">Button Ter</Button>*/}
-      {/*    </div>*/}
-      {/*  </header>*/}
-      {/*  <Image/>*/}
-      {/*  <SpeakerCard/>*/}
-      {/*  <Input/>*/}
-      {/*  <ScheduleCard/>*/}
-      {/*  <BoothBanner variant="quarter"/>*/}
-      {/*</div>*/}
+    <Router>
+      <div className="App">
+        <Switch>
+          <Redirect exact from={'/'} to={'/welcome'}/>
+          <Route exact path="/welcome" component={Welcome}/>
+          <Route exact path={'/dashboard'} component={Dashboard}/>
 
-      <Router>
-        <div className="App">
-          <Switch>
-            <Redirect exact from={'/'} to={'/welcome'}/>
-            <Route exact path="/welcome" component={Welcome}/>
-            <Route exact path={'/dashboard'} component={Dashboard}/>
-
-            {/*<Route exact path="/homepage" element={<Homepage />} />*/}
-            {/*<Route exact path="/event" element={<EventPage />} />*/}
-            {/*<Route exact path="/eventlanding" element={<EventLandingPage />} />*/}
-            {/*<Route exact path="/signin" element={<SignIn />} />*/}
-            {/*<Route exact path="/signup" element={<SignUp />} />*/}
-            {/*<Route exact path="/dashboard" element={<DashboardOverview />} />*/}
-            {/*<Route exact path="/dashboard/overview" element={<DashboardOverview />} />*/}
-            {/*<Route exact path="/dashboard/events" element={<DashboardEvents />} />*/}
-            {/*<Route exact path="/dashboard/create" element={<DashboardCreate />} />*/}
-            {/*<Route exact path="/dashboard/billing" element={<DashboardBilling />} />*/}
-            {/*<Route exact path="/dashboard/integration" element={<DashboardIntegration />} />*/}
-            {/*<Route exact path="/dashboard/events" element={<DashboardEvents />} />*/}
-            {/*<Route exact path="/dashboard/profile" element={<DashboardProfile />} />*/}
-            {/*<Route exact path="/dashboard/events/overview" element={<EventDashboardOverview />} />*/}
-            {/*<Route exact path="/dashboard/events/setup" element={<EventDashboardBasicSetup />} />*/}
-            {/*<Route exact path="/dashboard/events/registration" element={<EventDashboardRegistration />} />*/}
-            {/*<Route exact path="/dashboard/events/schedule" element={<EventDashboardSchedule />} />*/}
-            {/*<Route exact path="/dashboard/events/venue" element={<EventDashboardVenue />} />*/}
-            {/*<Route exact path="/dashboard/events/people" element={<EventDashboardPeople />} />*/}
-            {/*<Route exact path="/dashboard/events/marketing" element={<EventDashboardMarketing />} />*/}
-            {/*<Route exact path="/dashboard/events/recording" element={<EventDashboardRecording />} />*/}
-            {/*<Route exact path="/dashboard/events/analytics" element={<EventDashboardAnalytics />} />*/}
-            {/*<Route path="*" element={<NotFound />} />*/}
-          </Switch>
-        </div>
-      </Router>
-      <BoothBanner/>
-      <Image/>
-      <ScheduleCard/>
-      <SpeakerCard/>
-    </>
+          {/*<Route exact path="/homepage" element={<Homepage />} />*/}
+          {/*<Route exact path="/event" element={<EventPage />} />*/}
+          {/*<Route exact path="/eventlanding" element={<EventLandingPage />} />*/}
+          {/*<Route exact path="/signin" element={<SignIn />} />*/}
+          {/*<Route exact path="/signup" element={<SignUp />} />*/}
+          {/*<Route exact path="/dashboard" element={<DashboardOverview />} />*/}
+          {/*<Route exact path="/dashboard/overview" element={<DashboardOverview />} />*/}
+          {/*<Route exact path="/dashboard/events" element={<DashboardEvents />} />*/}
+          {/*<Route exact path="/dashboard/create" element={<DashboardCreate />} />*/}
+          {/*<Route exact path="/dashboard/billing" element={<DashboardBilling />} />*/}
+          {/*<Route exact path="/dashboard/integration" element={<DashboardIntegration />} />*/}
+          {/*<Route exact path="/dashboard/events" element={<DashboardEvents />} />*/}
+          {/*<Route exact path="/dashboard/profile" element={<DashboardProfile />} />*/}
+          {/*<Route exact path="/dashboard/events/overview" element={<EventDashboardOverview />} />*/}
+          {/*<Route exact path="/dashboard/events/setup" element={<EventDashboardBasicSetup />} />*/}
+          {/*<Route exact path="/dashboard/events/registration" element={<EventDashboardRegistration />} />*/}
+          {/*<Route exact path="/dashboard/events/schedule" element={<EventDashboardSchedule />} />*/}
+          {/*<Route exact path="/dashboard/events/venue" element={<EventDashboardVenue />} />*/}
+          {/*<Route exact path="/dashboard/events/people" element={<EventDashboardPeople />} />*/}
+          {/*<Route exact path="/dashboard/events/marketing" element={<EventDashboardMarketing />} />*/}
+          {/*<Route exact path="/dashboard/events/recording" element={<EventDashboardRecording />} />*/}
+          {/*<Route exact path="/dashboard/events/analytics" element={<EventDashboardAnalytics />} />*/}
+          {/*<Route path="*" element={<NotFound />} />*/}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/pages/testing/Dashboard.jsx
+++ src/pages/testing/Dashboard.jsx
@@ -1,14 +1,34 @@
-// import React from "react";
-import Sidebar from "../../layouts/Sidebar";
-const Dashboard = () => {
-  return(
+import React, { useState } from 'react';
+
+import Sidebar from '../../layouts/Sidebar';
+// import Header from '../../layouts/Header';
+
+function Dashboard() {
+
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
     <div className="flex h-screen overflow-hidden">
 
-      DASHBOARD
-    {/*<Sidebar />*/}
+      {/* Sidebar */}
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+      {/* Content area */}
+      <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+
+        {/*  Site header */}
+        {/*<Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />*/}
+
+        <main>
+          <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+
+          </div>
+        </main>
+
+      </div>
 
     </div>
-  )
+  );
 }
 
 export default Dashboard;
