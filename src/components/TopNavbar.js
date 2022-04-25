206407d5cb17f3e579ffcb65b9fefe4b4497bbe8
Nehemia Cecio <76204756+nehemiacecio@users.noreply.github.com>
2022-04-25 23:10:34 +0700

    Jwt Auth

--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -1,25 +1,47 @@
-import { createRef } from "react";
+import React, {createRef, useEffect, useState} from "react";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
 
 const TopNavbar = () => {
+  const [name, setName] = useState('');
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    refreshToken()
+  }, [])
+  const refreshToken = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/token');
+      setToken(response.data.accessToken);
+      const decoded = jwt_decode(response.data.accessToken)
+      console.log(decoded)
+    } catch (error) {
+
+    }
+  }
+
+
   const navContentRef = createRef();
 
   const navToggle = () => {
     navContentRef.current?.classList.toggle('hidden');
   }
 
+
   return (
     <nav className="bg-primary-500">
-      <div className="max-w-screen-2xl flex items-center justify-between flex-wrap px-8 sm:px-10 py-4 sm:py-5 mx-auto z-10 top-0">
+      <div
+        className="max-w-screen-2xl flex items-center justify-between flex-wrap px-8 sm:px-10 py-4 sm:py-5 mx-auto z-10 top-0">
         <a href="/" className="text-xl sm:text-2xl font-bold text-white">
           TEMU
         </a>
 
         <div className="block sm:hidden">
           <button onClick={navToggle}
-            className="flex items-center text-white">
+                  className="flex items-center text-white">
             <svg className="fill-current h-5" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
             </svg>
           </button>
         </div>
@@ -27,16 +49,25 @@ const TopNavbar = () => {
         {/* Navbar */}
 
         <div className="w-full hidden sm:flex sm:items-center sm:w-auto pt-6 sm:pt-0"
-          ref={navContentRef}>
+             ref={navContentRef}>
           <ul className="sm:flex justify-end flex-1 items-center">
             <li className="ml-0 sm:ml-6 font-semibold">
-              <a href="#services" className="inline-block py-2 sm:py-0 text-base md:text-lg text-white no-underline hover:text-secondary-500 transition">Services</a>
+              <a href="#services"
+                 className="inline-block py-2 sm:py-0 text-base md:text-lg text-white no-underline hover:text-secondary-500 transition">Services</a>
             </li>
             <li className="ml-0 sm:ml-6 font-semibold">
-              <a href="#live" className="inline-block py-2 sm:py-0 text-base md:text-lg text-white no-underline hover:text-secondary-500 transition">Experience Live</a>
+              <a href="#live"
+                 className="inline-block py-2 sm:py-0 text-base md:text-lg text-white no-underline hover:text-secondary-500 transition">Experience
+                Live</a>
             </li>
             <li className="ml-0 sm:ml-6 font-semibold">
-              <a href="#contact" className="inline-block py-2 sm:py-0 text-base md:text-lg text-white no-underline hover:text-secondary-500 transition">Contact Us</a>
+              <a href="#contact"
+                 className="inline-block py-2 sm:py-0 text-base md:text-lg text-white no-underline hover:text-secondary-500 transition">Contact
+                Us</a>
+            </li>
+            <li className="ml-0 sm:ml-6 font-semibold">
+              <a href="#"
+                 className="inline-block py-2 sm:py-0 text-base md:text-lg text-white no-underline hover:text-secondary-500 transition">Welcome,{name}</a>
             </li>
           </ul>
         </div>
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,18 +1,58 @@
+import React, {useState} from "react";
 import Button from "../components/Button"
 import Input from "../components/Input"
+import {useHistory} from "react-router-dom";
+import axios from "axios";
 
 const SignIn = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const history = useHistory()
+  const [msg, setMsg] = useState('')
+
+  const Auth = async (e) => {
+    e.preventDefault()
+    try {
+      await axios.post('http://localhost:5000/login', {
+        email: email,
+        password: password,
+      }).then(function (res) {
+        console.log(res)
+      })
+      history.push("/homepagefirst")
+    } catch (error) {
+      if (error.res) {
+        setMsg(error.response.data.msg)
+      }
+    }
+  }
+
   return (
     <>
       <div className="min-h-screen bg-from-600-to-500 flex flex-col justify-center items-center text-neutral-50">
         <p className="mb-6 text-2xl">Sign in to Cranium</p>
-        <form action="/signin" method="POST" className="w-80 text-sm text-center bg-neutral-800 border border-neutral-400 rounded-lg shadow-lg p-6">
-          <Input type="email" placeholder="Email" className="mb-4" />
-          <Input type="password" placeholder="Password" className="mb-4" />
+        <form action="/signin" method="POST"
+              onSubmit={Auth}
+              className="w-80 text-sm text-center bg-neutral-800 border border-neutral-400 rounded-lg shadow-lg p-6">
+          <p className="has-text-centered">{msg}</p>
+          <Input type="email" placeholder="Email" className="mb-4"
+                 value={email} onChange={(e) => {
+            setEmail(e.target.value)
+          }}
+          />
+          <Input type="password" placeholder="Password" className="mb-4"
+                 value={password} onChange={(e) => {
+            setPassword(e.target.value)
+          }}
+          />
           <Button size="large" type="submit" variant="primary" className="w-full mb-4">Sign in</Button>
-          <hr className="border-neutral-400 mb-4" />
-          <Button size="large"type="button" variant="primary" className="w-full mb-4 bg-neutral-50 border-neutral-50 hover:bg-neutral-300 hover:border-neutral-300 hover:shadow-neutral-600"><img className="w-10 mr-4" src="./images/jpeg/logogoogle.png"/>Sign in with Google</Button>
-          <p className="text-xs -mb-1">Don't have an account yet? <Button href="/signup" variant="tertiary" className="inline text-xs">Sign up</Button></p>
+          <hr className="border-neutral-400 mb-4"/>
+          <Button size="large" type="button" variant="primary"
+                  className="w-full mb-4 bg-neutral-50 border-neutral-50 hover:bg-neutral-300 hover:border-neutral-300 hover:shadow-neutral-600"><img
+            className="w-10 mr-4" src="./images/jpeg/logogoogle.png"/>Sign in with Google</Button>
+          <p className="text-xs -mb-1">Don't have an account yet? <Button href="/signup" variant="tertiary"
+                                                                          className="inline text-xs">Sign up</Button>
+          </p>
         </form>
       </div>
     </>
