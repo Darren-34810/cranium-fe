6e2ac4aa86bf8d3a242df3225b4e66fc332fa16c
Darren-34810 <darren.denisson@student.umn.ac.id>
2022-04-26 14:06:45 +0700

    SignIn and SignUp Page Mastering

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,5 @@
-import Button from "../components/Button"
-import Input from "../components/Input"
+import Button from "../components/Button";
+import Input from "../components/Input";
 import { useState } from "react";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
@@ -7,21 +7,21 @@ import { useHistory } from "react-router-dom";
 const SignUp = () => {
   // const [firstName, setFirstName] = useState('')
   // const [lastName, setLastName] = useState('')
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   // const [number, setNumber] = useState('')
-  const [password, setPassword] = useState('');
-  const [confPassword, setConfPassword] = useState('');
-  const [msg, setMsg] = useState('');
-  const history = useHistory()
+  const [password, setPassword] = useState("");
+  const [confPassword, setConfPassword] = useState("");
+  const [msg, setMsg] = useState("");
+  const history = useHistory();
 
   const SignUpForm = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      console.log(name)
-      console.log(email)
-      console.log(password)
-      console.log(confPassword)
+      console.log(name);
+      console.log(email);
+      console.log(password);
+      console.log(confPassword);
       // await axios.post('http://localhost:5000/users', {
       //   name: name,
       //   email: email,
@@ -33,78 +33,115 @@ const SignUp = () => {
       // history.push("/signin")
     } catch (error) {
       if (error.res) {
-        setMsg(error.response.data.msg)
+        setMsg(error.response.data.msg);
       }
     }
-  }
+  };
   return (
     <>
-      <div className="min-h-screen bg-from-600-to-500 flex justify-center items-center text-neutral-50 transition-all">
+      <div className="min-h-screen bg-primary-800 flex justify-center items-center text-neutral-50 transition-all">
         <div className="max-w-4xl flex flex-col md:flex-row justify-evenly items-center py-16 md:py-0">
-          <div className="w-[85%] md:w-2/5 mb-16 md:mb-0 flex flex-col justify-center">
-            <p className="w-full mb-6 text-3xl font-bold">Sign up to Cranium</p>
-            <p className="mb-4">Ciptakan pengalaman unik dan bermakna dengan menjelajahi virtual space milik Anda</p>
+          <div className="w-[85%] md:w-2/5 mb-10 md:mb-0 flex flex-col justify-center">
+            <div className="flex flex-col lg:flex-row justify-center items-center lg:items-stretch mt-8">
+              <div className="hl lg:vl"></div>
+              <h2 className="text-neutral-50 text-2xl sm:text-4xl font-bold py-1 lg:mr-8 xl:mr-0">
+                Sign Up to TEMU-
+              </h2>
+            </div>
+            <p className="mt-8 mb-4">
+              Ciptakan pengalaman unik dan bermakna dengan menjelajahi virtual
+              space milik Anda
+            </p>
             <div>
               <p>Mengapa menggunakan virtual reality?</p>
               <ul className="list-disc ml-5">
                 <li>Membuat acara hybrid yang variatif</li>
-                <li>Meningkatkan konversi dalam acara untuk mendukung tujuan bisnis/event</li>
+                <li>
+                  Meningkatkan konversi dalam acara untuk mendukung tujuan
+                  bisnis/event
+                </li>
                 <li>Memperoleh data yang lebih relevan dengan lebih mudah</li>
               </ul>
             </div>
           </div>
           <div className="w-full md:w-2/5 flex justify-center">
-            <form action="/signup" method="POST"
+            <form
+              action="/signup"
+              method="POST"
               className="w-80 text-sm text-center bg-neutral-800 border border-neutral-400 rounded-lg shadow-lg p-6"
               onSubmit={SignUpForm}
             >
               <p className="has-text-centered">{msg}</p>
-              {/*<Input type="text" placeholder="First Name" className="mb-4" value={firstName} onChange={(e) => {*/}
+              {/*<Input type="text" placeholder="First Name" className="border-none mb-4" value={firstName} onChange={(e) => {*/}
               {/*  setFirstName(e.target.value)*/}
               {/*}}/>*/}
-              {/*<Input type="text" placeholder="Last Name" className="mb-4"*/}
+              {/*<Input type="text" placeholder="Last Name" className="border-none mb-4"*/}
               {/*       value={lastName} onChange={(e) => {*/}
               {/*  setLastName(e.target.value)*/}
               {/*}}/>*/}
 
-              {/* <Input type="text" placeholder="Name" className="mb-4" */}
+              {/* <Input type="text" placeholder="Name" className="border-none mb-4" */}
               {/* value={name} onChange={(e) => {setName(e.target.value)}} /> */}
 
               <Input
                 type="text"
                 placeholder="Name"
-                className="mb-4"
+                className="border-none mb-4"
                 value={name}
                 inputHandler={(valueCallback) => setName(valueCallback)}
               />
 
-              {/*<Input type="text" placeholder="Phone Number" className="mb-4"*/}
+              {/*<Input type="text" placeholder="Phone Number" className="border-none mb-4"*/}
               {/*       value={number} onChange={(e) => {*/}
               {/*  setNumber(e.target.value)*/}
               {/*}}/>*/}
-              <Input type="email" placeholder="Email" className="mb-4"
-                value={email} inputHandler={(valueCallback) => setEmail(valueCallback)} />
+              <Input
+                type="email"
+                placeholder="Email"
+                className="border-none mb-4"
+                value={email}
+                inputHandler={(valueCallback) => setEmail(valueCallback)}
+              />
 
-              <Input type="password" placeholder="Password" className="mb-4"
-                value={password} inputHandler={(valueCallback) => setPassword(valueCallback)} />
-              <Input type="password" placeholder=" Confirm Password" className="mb-4"
-                value={confPassword} inputHandler={(valueCallback) => setConfPassword(valueCallback)} />
+              <Input
+                type="password"
+                placeholder="Password"
+                className="border-none mb-4"
+                value={password}
+                inputHandler={(valueCallback) => setPassword(valueCallback)}
+              />
+              <Input
+                type="password"
+                placeholder=" Confirm Password"
+                className="border-none mb-4"
+                value={confPassword}
+                inputHandler={(valueCallback) => setConfPassword(valueCallback)}
+              />
 
-              <Button size="large" type="submit" variant="primary" className="w-full mb-4">Sign Up</Button>
-              <hr className="border-neutral-400 mb-4" />
-
-              <Button size="large" type="button" variant="primary"
-                className="w-full mb-4 bg-neutral-50 border-neutral-50 hover:bg-neutral-300 hover:border-neutral-300 hover:shadow-neutral-600"><img
-                  className="w-10 mr-4" src="./images/jpeg/logogoogle.png" />Sign in with Google</Button>
-              <p className="text-xs -mb-1">Already have an account? <Button href="/signin" variant="tertiary"
-                className="inline text-xs">Sign in</Button>
+              <Button
+                size="large"
+                type="submit"
+                variant="primary"
+                className="w-full mb-4"
+              >
+                Sign Up
+              </Button>
+              <p className="text-xs -mb-1">
+                Already have an account?{" "}
+                <Button
+                  href="/signin"
+                  variant="tertiary"
+                  className="inline text-xs"
+                >
+                  Sign in
+                </Button>
               </p>
             </form>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
